95d6d4131335d46c17be97cc3b553cd2d6653042
Ashenafi Bancha <156710615+Ashenafi-Bancha@users.noreply.github.com>
2025-02-14 00:42:23 +0300

    add a getAverage function.

--- gradeBoodApp.js
+++ gradeBoodApp.js
@@ -1,1 +1,13 @@
+//function which calculates the students averagee based on their total scores and the number of subjects which they learn.
+function getAverage(scores) {
+  let sum = 0;
 
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
+// code to print(log)  the average  score of student to the console.
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
