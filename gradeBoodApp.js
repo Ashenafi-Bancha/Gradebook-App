04a3723bd734dfa55bca4198c9920f74dc600585
Ashenafi Bancha <156710615+Ashenafi-Bancha@users.noreply.github.com>
2025-02-14 22:49:06 +0300

    Update gradeBoodApp.js

--- gradeBoodApp.js
+++ gradeBoodApp.js
@@ -11,3 +11,19 @@ function getAverage(scores) {
 // code to print(log)  the average  score of student to the console.
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+// grade calculating function
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
